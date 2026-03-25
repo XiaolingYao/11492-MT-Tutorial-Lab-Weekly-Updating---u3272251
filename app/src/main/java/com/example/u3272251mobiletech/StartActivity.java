5187c9c8ef738850729dc7107bc9839039897967
Xiaoling Yao <u3272251@uni.canberra.edu.au>
2026-03-25 18:43:02 +1100

    11492 MT week 6 lab

--- app/src/main/java/com/example/u3272251mobiletech/StartActivity.java
+++ app/src/main/java/com/example/u3272251mobiletech/StartActivity.java
@@ -42,5 +42,14 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button buttonMLKit = findViewById(R.id.buttonMLKitEvent);
+        buttonMLKit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, MLKitActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
